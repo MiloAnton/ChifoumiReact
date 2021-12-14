ff69edc4e2d4ccf71b15c5df9e852a7742ef6e49
MiloAnton <milo.roche@outlook.com>
2021-12-14 16:43:19 +0100

    Commit fonctionnel code moche

--- src/App.js
+++ src/App.js
@@ -2,18 +2,36 @@ import './App.css';
 import Button from './components/Button';
 
 function App() {
+  const signes = ['👊', '👋', '✌️'];
   return (
     <div className="App">
       <header className="App-header">
-          <Button value={'👋'} />
-          <Button value={'✌️'} />
-          <Button value={'👊'} />
+          <Button value={'👊'} onClick={() => playGame(0)}/>
+          <Button value={'👋'} onClick={() => playGame(1)}/>
+          <Button value={'✌️'} onClick={() => playGame(2)}/>
       </header>
     </div>
   );
 }
 export default App;
 
-function startGame() {
-  
+function playGame(played) {
+  const possible = ['👊', '👋', '✌️'];
+  let random = Math.floor(Math.random() * possible.length);
+  console.log(random);
+  if (random == played ) {
+    window.alert("Square game...");
+  } else if ( random == 0 && played == 1) { 
+    window.alert("You win, bravo le papier");
+  } else if ( random == 0 && played == 2) {
+    window.alert("You don't win, pas bravo les ciseaux");
+  } else if ( random == 1 && played == 0) {
+    window.alert("You are vraiment pas bon, dommage le fist");
+  } else if ( random == 1 && played == 2) {
+    window.alert("Wow t'es fort tagagné, bien les ciseaux");
+  } else if ( random == 2 && played == 1) {
+    window.alert("Bof/20... Papier pas solide");
+  } else if ( random == 2 && played == 0) {
+    window.alert("Tagagné, tétrofor, comme ta main serrée");
+  } 
 }
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,10 @@
 import React from "react";
 import './button.css';
 
-export default function Button({ value }) {
+export default function Button({ onClick:player, value }) {
     return ( 
         <button 
+            onClick={player}
             className="button-30"
         >
             {value}
