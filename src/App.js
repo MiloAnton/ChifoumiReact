c062a46562dedea547425bdbcd4a2232e41f34a0
MiloAnton <milo.roche@outlook.com>
2022-01-19 17:54:11 +0100

    Added toasts

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App() {
   const [scoreH, setScoreH] = useState(0);
   const [scoreC, setScoreC] = useState(0);
-  
+  const notifyW = () => toast.success("Yes yes 1 pt for you...", {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+  const notifyL = () => toast.error("computer === better", {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    });
+
+
   function resetScore() {
     setScoreC(scoreC - scoreC);
     setScoreH(scoreH - scoreH);
@@ -17,22 +38,40 @@ export default function App() {
     console.log(random);
     if (random == played) {
     } else if (random == 0 && played == 1) {
+        notifyW();
         setScoreH(scoreH + 1);
     } else if (random == 0 && played == 2) {
+        notifyL();
         setScoreC(scoreC + 1);
     } else if (random == 1 && played == 0) {
+        notifyL();
         setScoreC(scoreC + 1);
     } else if (random == 1 && played == 2) {
+        notifyW();
         setScoreH(scoreH + 1);
     } else if (random == 2 && played == 1) {
+        notifyL();
         setScoreC(scoreC + 1);
     } else if (random == 2 && played == 0) {
+        notifyW();
         setScoreH(scoreH + 1);
     }
   }
 
   return (
     <div className="App">
+      <ToastContainer
+            toastStyle={{ backgroundColor: '#36494c', color : 'white' }}
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+          />
       <div className="Scoreboard">
         {scoreC} &nbsp;💻 &nbsp;&nbsp;&nbsp;&nbsp;Scoreboard&nbsp;&nbsp;&nbsp;&nbsp; 👶 &nbsp;{scoreH}
       </div>
