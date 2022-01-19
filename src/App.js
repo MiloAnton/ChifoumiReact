cb821a4438016532e83b5e88074b09f234a77a00
MiloAnton <milo.roche@outlook.com>
2022-01-19 16:02:23 +0100

    Scoreboard fixed (no CSS)

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+import { useState } from 'react';
 import './App.css';
 import Button from './components/Button';
-import Scoreboard from './components/scoreboard';
-
-let scoreHuman = 0;
-let scoreCPU = 0;
 
 export default function App() {
-  const signes = ['👊', '👋', '✌️'];
+  const [scoreH, setScoreH] = useState(0);
+  const [scoreC, setScoreC] = useState(0);
+  
+  function resetScore() {
+    setScoreC(scoreC - scoreC);
+    setScoreH(scoreH - scoreH);
+  }
+  
+  function playGame(played) {
+    const possible = ['👊', '👋', '✌️'];
+    let random = Math.floor(Math.random() * possible.length);
+    console.log(random);
+    if (random == played) {
+    } else if (random == 0 && played == 1) {
+        setScoreH(scoreH + 1);
+    } else if (random == 0 && played == 2) {
+        setScoreC(scoreC + 1);
+    } else if (random == 1 && played == 0) {
+        setScoreC(scoreC + 1);
+    } else if (random == 1 && played == 2) {
+        setScoreH(scoreH + 1);
+    } else if (random == 2 && played == 1) {
+        setScoreC(scoreC + 1);
+    } else if (random == 2 && played == 0) {
+        setScoreH(scoreH + 1);
+    }
+  }
+
   return (
     <div className="App">
-      <div>
-        <Scoreboard />
-      </div>
       <header className="App-header">
+          <div>
+            {scoreC} 💻 Scoreboard 👶 {scoreH}
+          </div>
           <Button value={'👊'} onClick={() => playGame(0)}/>
           <Button value={'👋'} onClick={() => playGame(1)}/>
           <Button value={'✌️'} onClick={() => playGame(2)}/>
@@ -20,38 +44,4 @@ export default function App() {
       </header>
     </div>
   );
-}
-
-function resetScore() {
-  scoreHuman=0;
-  scoreCPU=0;
-  Scoreboard(scoreHuman, scoreCPU)
-}
-
-function playGame(played) {
-  const possible = ['👊', '👋', '✌️'];
-  let random = Math.floor(Math.random() * possible.length);
-  console.log(random);
-  if (random == played) {
-      //window.alert("Pffff égalité...");
-  } else if (random == 0 && played == 1) {
-      scoreHuman++;
-      //window.alert("You win, bravo le papier");
-  } else if (random == 0 && played == 2) {
-      scoreCPU++;
-      //window.alert("You don't win, pas bravo les ciseaux");
-  } else if (random == 1 && played == 0) {
-      scoreCPU++;
-      //window.alert("You are vraiment pas bon, dommage le fist");
-  } else if (random == 1 && played == 2) {
-      scoreHuman++;
-      //window.alert("Wow t'es fort tagagné, bien les ciseaux");
-  } else if (random == 2 && played == 1) {
-      scoreCPU++;
-      //window.alert("Bof/20... Papier pas solide");
-  } else if (random == 2 && played == 0) {
-      scoreHuman++;
-      //window.alert("Tagagné, tétrofor, comme ta main serrée");
-  }
-  Scoreboard(scoreHuman, scoreCPU)
 }
--- src/components/scoreboard.js
+++
@@ -1,9 +0,0 @@
-export default function Scoreboard(scoreHuman, scoreCPU) {
-    console.log("Player 1 : " + scoreHuman);
-    console.log("Player 2 : " + scoreCPU);
-    return (
-        <p>
-            scoreHuman Scoreboard scoreCPU
-        </p>
-    );
-}
